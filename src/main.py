614bb28a0d7115e3bf3d9462d9afa39c42c9059a
Antônio Carlos Durães Da Silva <20162BSI0244@cefetes.br>
2019-10-03 11:36:31 -0300

    backup

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from os import path
 
 class Usuario():
 
@@ -13,14 +13,24 @@ class Usuario():
 		self.altura = altura
 		self.peso = peso
 
+	def __str__():
+		return f"email: '{self.email}'; genero: '{self.genero}'; uuid: '{self.uuid}'; data_nasc: {self.data_nasc}; altura: {self.altura}; peso: {self.peso};"
+
 #TODO: finalizar
-def convert_linha_usuario(linha: str) -> Usuario:
-	usuario = None
+def convert_linha_usuario(separador: str, linha: str) -> Usuario:
+	params = linha.split(separador)
+	usuario: Usuario = Usuario(params[0], params[1], params[2], params[3], float(params[4]), float(params[5]))
 	return usuario
 
 #TODO: finalizar
-def convert_arq_para_usuarios(arq_caminho: str) -> [Usuario]:
-	return None
+def convert_arq_para_usuarios(arq_caminho: str, separador: str) -> [Usuario]:
+
+	usuarios: [Usuario]
+
+	with open(arq_caminho, 'r', encoding='utf-8') as arq:
+		usuarios = [convert_linha_usuario(separador, linha) for i, linha in enumerate(arq.readlines()) if i > 0]
+
+	return usuarios
 
 #TODO: Implementar comparable
 def main(args: [str]):
@@ -29,8 +39,10 @@ def main(args: [str]):
 	# parseOutputFileName(args)
 
 	# A ← readCsv(inputName)
-	arq_path = args[2]
-	users = convert_arq_para_usuarios(arq_path)
+	print(path.curdir)
+	arq_path =  'data_75e4.csv'
+	usuarios = convert_arq_para_usuarios(arq_path, ',')
+	[print(u + '\n') for u in usuarios]
 
 	# initTime ← getCpuTime
 	# xyzSort(A)
